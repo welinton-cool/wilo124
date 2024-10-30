3da5c962a503f1e71c6e20c8853158ee9698cdef
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-30 10:08:29 -0500

    practicas

---
+++ src/main/java/ec/edu/ec/wilo123/Wilo123.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.wilo123;
+
+/**
+ *
+ * @author HP
+ */
+public class Wilo123 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
